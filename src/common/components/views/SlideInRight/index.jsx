e9d3320a83db0623e709523b160257629e9516fc
kiki le singe <aalbertini95@gmail.com>
2016-06-16 23:46:19 +0200

    feat(SlideInRight): add SlideInRight view

---
+++ src/common/components/views/SlideInRight/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function SlideInRight() {
+  return (
+    <div className="page page-slide-in-right">
+      <div className="page-content">
+        <h1>SlideInRight</h1>
+      </div>
+    </div>
+  )
+}
