e0da2bbef0b83a00b788bd90beaeb6008dca3706
kiki le singe <aalbertini95@gmail.com>
2016-06-29 17:21:18 +0200

    feat(AppLayout): move Home view as default view

--- src/common/layouts/AppLayout/index.jsx
+++ src/common/layouts/AppLayout/index.jsx
@@ -2,6 +2,8 @@ import React, { PropTypes, cloneElement } from 'react'
 import { Link } from 'react-router'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
+import Home from 'common/components/views/Home'
+
 // *** STYLES *** //
 import 'common/styles/global/core.css'
 
@@ -16,11 +18,13 @@ function AppLayout(props) {
   return (
     <div className="views">
       <ul>
-        <li><Link to="/home">Home</Link></li>
         <li><Link to="/hello">Hello</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/slide-in-right">SlideInRight</Link></li>
       </ul>
+
+      <Home />
+
       <ReactCSSTransitionGroup
         transitionName="slideInRight"
         transitionEnterTimeout={400}
--- src/common/routes/index.jsx
+++ src/common/routes/index.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
-import { Route, IndexRoute, Redirect } from 'react-router'
+import { Route, Redirect } from 'react-router'
 
 import AppLayout from 'common/layouts/AppLayout'
-import Home from 'common/components/views/Home'
 import About from 'common/components/views/About'
 import Hello from 'common/components/views/Hello'
 import SlideInRight from 'common/components/views/SlideInRight'
 
 export default (
   <Route path="/" component={AppLayout}>
-    <IndexRoute component={Home} />
-    <Route path="home" component={Home} />
     <Route path="hello" component={Hello} />
     <Route path="about" component={About} />
     <Route path="slide-in-right" component={SlideInRight} />
