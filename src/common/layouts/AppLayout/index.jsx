198fa520ee8d3a9836c7123cb2105ef73d6ae3d0
kiki le singe <aalbertini95@gmail.com>
2016-06-29 17:39:20 +0200

    feat(AppLayout): set transition name on click

--- src/common/layouts/AppLayout/index.jsx
+++ src/common/layouts/AppLayout/index.jsx
@@ -12,30 +12,47 @@ const propTypes = {
   children: PropTypes.object
 }
 
-  const { children, location } = props
-
-  return (
-    <div className="views">
-      <ul>
-        <li><Link to="/hello">Hello</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/slide-in-right">SlideInRight</Link></li>
-      </ul>
-
-      <Home />
-
-      <ReactCSSTransitionGroup
-        transitionName="slideInRight"
-        transitionEnterTimeout={400}
-        transitionLeaveTimeout={400}
-      >
-        {children && cloneElement(children, {
-          key: location.pathname
-        })}
-      </ReactCSSTransitionGroup>
-    </div>
-  )
 class AppLayout extends Component {
+  constructor() {
+    super()
+
+    this.state = { transition: '' }
+
+    this.setTransitionName = this.setTransitionName.bind(this)
+  }
+
+  setTransitionName(transition = '') {
+    this.setState({ transition })
+  }
+
+  render() {
+    const { children, location } = this.props
+
+    /* eslint-disable */
+    return (
+      <div className="views">
+        <ul>
+          <li><Link to="/hello">Hello</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li>
+            <Link to="/slide-in-right" onClick={this.setTransitionName.bind(this, 'slideInRight')}>SlideInRight</Link>
+          </li>
+        </ul>
+
+        <Home />
+
+        <ReactCSSTransitionGroup
+          transitionName={this.state.transition}
+          transitionEnterTimeout={400}
+          transitionLeaveTimeout={400}
+        >
+          {children && cloneElement(children, {
+            key: location.pathname
+          })}
+        </ReactCSSTransitionGroup>
+      </div>
+    )
+  }
 }
 
 AppLayout.propTypes = propTypes
