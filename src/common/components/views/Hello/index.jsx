ae1640152663671b20837b154cd48ebfae93cd4e
kiki le singe <aalbertini95@gmail.com>
2016-07-15 17:09:47 +0200

    feat(Hello): make `Hello` page as `slideInUp` view

--- src/common/components/views/Hello/index.jsx
+++ src/common/components/views/Hello/index.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 
-export default function Hello() {
+const contextTypes = {
+  router: PropTypes.object
+}
+
+export default function Hello(props, context) {
+  const closeView = () => {
+    context.router.push('/')
+  }
+
   return (
-    <div className="page page-hello">
+    <div className="page page-slide-in-up page-hello">
+      <header>
+        <h1>Hello</h1>
+        <button onClick={closeView}>Close</button>
+      </header>
       <div className="page-content">Hello <span className="text">World!</span></div>
     </div>
   )
 }
+
+Hello.contextTypes = contextTypes
