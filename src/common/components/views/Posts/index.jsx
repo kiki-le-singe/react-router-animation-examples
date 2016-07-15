4b6c9f9ef4bd537ae75fdc0480ee5f5a46709f63
kiki le singe <aalbertini95@gmail.com>
2016-07-15 17:06:35 +0200

    feat(Post): add Post view

---
+++ src/common/components/views/Posts/index.jsx
@@ -0,0 +1,52 @@
+import React, { PropTypes, cloneElement } from 'react'
+import { Link } from 'react-router'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+
+const propTypes = {
+  location: PropTypes.object,
+  children: PropTypes.object
+}
+
+const contextTypes = {
+  router: PropTypes.object
+}
+
+export default function Posts(props, context) {
+  const closeView = () => {
+    context.router.push('/')
+  }
+  const { children, location } = props
+
+  return (
+    <div className="page page-slide-in-right page-posts">
+      <header>
+        <h1>Posts</h1>
+        <button onClick={closeView}>Close</button>
+      </header>
+      <section className="content posts">
+        <ul>
+          <li><Link to="posts/post-1">Post 1</Link></li>
+          <li><Link to="posts/post-2">Post 2</Link></li>
+          <li><Link to="posts/post-3">Post 3</Link></li>
+          <li><Link to="posts/post-4">Post 4</Link></li>
+          <li><Link to="posts/post-5">Post 5</Link></li>
+        </ul>
+      </section>
+
+      <ReactCSSTransitionGroup
+        transitionName="slideInRight"
+        transitionAppear
+        transitionAppearTimeout={400}
+        transitionEnterTimeout={400}
+        transitionLeaveTimeout={400}
+      >
+        {children && cloneElement(children, {
+          key: location.pathname
+        })}
+      </ReactCSSTransitionGroup>
+    </div>
+  )
+}
+
+Posts.propTypes = propTypes
+Posts.contextTypes = contextTypes
