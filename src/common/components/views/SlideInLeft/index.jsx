3be8295e418eabf3d8f5a03978147b2757c0a95b
kiki le singe <aalbertini95@gmail.com>
2016-07-15 18:44:27 +0200

    feat(SlideInLeft): implement nested animations

--- src/common/components/views/SlideInLeft/index.jsx
+++ src/common/components/views/SlideInLeft/index.jsx
@@ -1,4 +1,11 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes, cloneElement } from 'react'
+import { Link } from 'react-router'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+
+const propTypes = {
+  location: PropTypes.object,
+  children: PropTypes.object
+}
 
 const contextTypes = {
   router: PropTypes.object
@@ -8,6 +15,7 @@ export default function SlideInLeft(props, context) {
   const closeView = () => {
     context.router.push('/')
   }
+  const { children, location } = props
 
   return (
     <div className="page page-slide-in-left">
@@ -15,8 +23,25 @@ export default function SlideInLeft(props, context) {
         <h1>SlideInLeft</h1>
         <button onClick={closeView}>Close</button>
       </header>
+
+      <ul>
+        <li><Link to="/slide-in-left/tab1">Tab 1</Link></li>
+        <li><Link to="/slide-in-left/tab2">Tab 2</Link></li>
+      </ul>
+
+      <ReactCSSTransitionGroup
+        component="div"
+        transitionName="fadeIn"
+        transitionEnterTimeout={400}
+        transitionLeaveTimeout={400}
+      >
+        {children && cloneElement(children, {
+          key: location.pathname
+        })}
+      </ReactCSSTransitionGroup>
     </div>
   )
 }
 
+SlideInLeft.propTypes = propTypes
 SlideInLeft.contextTypes = contextTypes
