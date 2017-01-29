97fe764a8f18203572802dfb29f4f72fa0e9aae1
kiki le singe <aalbertini95@gmail.com>
2017-01-29 23:39:10 +0100

    refactor(config.dev): update dev config

--- src/common/config/dev.js
+++ src/common/config/dev.js
@@ -2,9 +2,9 @@ export default {
   app: {
     htmlAttributes: { lang: 'en' },
     title: 'React Redux Universal Boilerplate',
-    titleTemplate: 'React Redux Universal Boilerplate - %s',
+    titleTemplate: 'React Router Animation Examples - %s',
     meta: [
-      { name: 'description', content: 'Another React/Redux universal boilerplate.' },
+      { name: 'description', content: 'Simple example using React\'s Animations with React Router.' },
     ],
   },
 }
