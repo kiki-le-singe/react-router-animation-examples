aa33ec1e3d4a7e957c827be9cbae490ac20693be
kiki le singe <aalbertini95@gmail.com>
2017-01-29 23:41:08 +0100

    style(HomeVIew): fix linter

--- src/common/views/HomeView/HomeView.jsx
+++ src/common/views/HomeView/HomeView.jsx
@@ -1,29 +1,15 @@
-import React, { PureComponent, PropTypes } from 'react'
+import React from 'react'
 
 // require the logo image both from client and server
 const logoImage = require('./kiki.jpg')
 
-export default class HomeView extends PureComponent {
-  // static propTypes = {
-  //   setTransitionName: PropTypes.func
-  // }
-  ///
-  // static defaultProps = {
-  //   setTransitionName: () => {}
-  // }
-  ///
-  // componentDidMount() {
-  //   this.props.setTransitionName('fadeIn')
-  // }
-
-  render() {
-    return (
-      <div className="page page-home">
-        <div className="page-content">
-          <h2>Home</h2>
-          <p><img src={logoImage} alt="Kiki" width="300" height="300" /></p>
-        </div>
+export default function HomeView() {
+  return (
+    <div className="page page-home">
+      <div className="page-content">
+        <h2>Home</h2>
+        <p><img src={logoImage} alt="Kiki" width="300" height="300" /></p>
       </div>
-    )
-  }
+    </div>
+  )
 }
